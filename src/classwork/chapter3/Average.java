a87e17857ba81fe15389b4b279479378274b15d0
admin <Lilitkarapetyan.221715@gmail.com>
2023-08-25 01:08:16 +0300

    classwork Array

---
+++ src/classwork/chapter3/Average.java
@@ -0,0 +1,12 @@
+package classwork.chapter3;
+
+public class Average {
+    public static void main(String[] args) {
+        double nums[] = {10.1, 11.2, 12.3, 13.4, 14.5,};
+        double result = 0;
+        int i;
+        for (i = 0; i<0; i++);
+        result = result + nums[i];
+        System.out.println("average is " + result / 5);
+    }
+}
