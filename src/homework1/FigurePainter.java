888a2f4cfd17ced9ecf39e05b55916dfa5935cbf
admin <Lilitkarapetyan.221715@gmail.com>
2023-08-28 10:04:04 +0300

    Homework1

---
+++ src/homework1/FigurePainter.java
@@ -0,0 +1,37 @@
+package homework1;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+        for (int i=0; i < 0; i++);
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println();
+        System.out.println();
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+        System.out.println();
+        System.out.println();
+        System.out.println(" " + " " + " " + " " + "*");
+        System.out.println(" " + " " + " " + "**");
+        System.out.println(" " + " " + "***");
+        System.out.println(" " + "****");
+        System.out.println("*****");
+        System.out.println();
+        System.out.println();
+        System.out.println("*****");
+        System.out.println(" " + "****");
+        System.out.println(" " + " " + "***");
+        System.out.println(" " + " " + " " + "**");
+        System.out.println(" " + " " + " " + " " + "*");
+
+    }
+
+
+
+}
