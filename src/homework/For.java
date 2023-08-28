64bd2c5af99d41833d0acd2aed26655e2c8786ad
admin <Lilitkarapetyan.221715@gmail.com>
2023-08-28 16:47:27 +0300

    Home1

---
+++ src/homework/For.java
@@ -0,0 +1,20 @@
+package homework;
+
+public class For {
+    public static void main(String[] args) {
+        //for (int i = 0; i < 5; i++) {
+          //  for (int j = 0; j < 4; j++)
+            //    System.out.print("poxos");
+            //System.out.println();
+        //}
+
+        for (int i = 0; i < 1; i++) {
+            System.out.println("*\n**\n***\n****\n*****");
+            System.out.println();
+            System.out.println();
+            System.out.println("*****\n****\n***\n**\n*");
+
+
+        }
+    }
+}
--- src/homework/Homework1.java
+++ src/homework/Homework1.java
@@ -2,8 +2,8 @@ package homework;
 
 public class Homework1 {
     public static void main(String[] args) {
-        for (int i = 0; i < 6; i++) {
-            System.out.println("******");
+         for (int i = 0; i < 6; i++) {
+          System.out.println("******");
 
         }
 
