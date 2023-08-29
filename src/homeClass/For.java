186fe1f7cd1481b0b167406ef670ae0b2dbef986
admin <Lilitkarapetyan.221715@gmail.com>
2023-08-29 12:30:13 +0300

    HomeClass

--- src/homeClass/For.java
+++ src/homeClass/For.java
@@ -1,4 +1,4 @@
-package homework;
+package homeClass;
 
 public class For {
     public static void main(String[] args) {
--- src/homeClass/Homework1.java
+++ src/homeClass/Homework1.java
@@ -1,4 +1,4 @@
-package homework;
+package homeClass;
 
 public class Homework1 {
     public static void main(String[] args) {
