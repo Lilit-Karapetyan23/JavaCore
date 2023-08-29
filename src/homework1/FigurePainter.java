857660d7d0da033376b201da9d4fee88acd07305
admin <Lilitkarapetyan.221715@gmail.com>
2023-08-29 12:24:23 +0300

    Homework

--- src/homework1/FigurePainter.java
+++ src/homework1/FigurePainter.java
@@ -2,7 +2,7 @@ package homework1;
 
 public class FigurePainter {
     public static void main(String[] args) {
-        for (int i=0; i < 0; i++);
+        for (int i = 0; i < 0; i++) ;
         System.out.println("*");
         System.out.println("**");
         System.out.println("***");
@@ -31,7 +31,6 @@ public class FigurePainter {
         System.out.println(" " + " " + " " + " " + "*");
 
     }
-
 
 
 }
